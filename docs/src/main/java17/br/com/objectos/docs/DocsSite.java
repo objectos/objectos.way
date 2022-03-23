8f36671f74bcdbdbcd39bea17183f1a575872797
Marcio Endo <marcio@objectos.com.br>
2022-03-23 10:03:33 -0300

    [#4] create the (empty) DocsSite class
    $source=2a08ad9339b62f2b0839e183d79cedd634d020e1$

---
+++ docs/src/main/java17/br/com/objectos/docs/DocsSite.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2022 Objectos Software LTDA.
+ *
+ * Reprodução parcial ou total proibida.
+ */
+package br.com.objectos.docs;
+
+import br.com.objectos.be.site.AbstractSite;
+
+public class DocsSite extends AbstractSite {
+
+  @Override
+  protected final void configure() {}
+
+}
