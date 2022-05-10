7b0a87a994ce5797c61ab12dc3e3aed8479439ce
Marcio Endo <marcio@objectos.com.br>
2022-05-10 10:19:22 -0300

    [#37] refactor Event2 (StackWalker)
    re objectos/objectos#37
    
    $source=bac29047fbb17bd04dd243c8d0c265881c5194ca$

--- logging/src/main/java/objectos/logging/Event2.java
+++ logging/src/main/java/objectos/logging/Event2.java
@@ -18,7 +18,7 @@ package objectos.logging;
 /**
  * A log event that takes two arguments.
  *
- * @param <T1> the type of the first log argument
+ * @param <T1, T2> the type of the first log argument
  * @param <T2> the type of the second log argument
  */
 public final class Event2<T1, T2> extends Event {
@@ -36,6 +36,141 @@ public final class Event2<T1, T2> extends Event {
    */
   public Event2(Class<?> source, String key, Level level) {
     super(source, key, level);
+  }
+
+  /**
+   * Creates a new event instance.
+   *
+   * @param source
+   *        a name to identify the source class this event is bound to
+   * @param key
+   *        a key that uniquely identifies this event within the given
+   *        {@code source}
+   * @param level
+   *        the logging level of this event
+   */
+  public Event2(String source, Object key, Level level) {
+    super(source, key, level);
+  }
+
+  /**
+   * Creates a new DEBUG log event.
+   *
+   * <p>
+   * The {@code source} and the {@code key} values are inferred from the method
+   * immediately calling this method.
+   *
+   * <p>
+   * The <strong>{@code source}</strong> value is the binary name of class
+   * enclosing the method calling this method.
+   *
+   * <p>
+   * The <strong>{@code key}</strong> value is a string containing the file
+   * name, a colon ':' and the line number where this method has been invoked.
+   *
+   * @param <T1> the type of the log argument
+   * @param <T2> the type of the second log argument
+   *
+   * @return a new DEBUG log event
+   */
+  public static <T1, T2> Event2<T1, T2> debug() {
+    return create(Level.DEBUG, Event2::new);
+  }
+
+  /**
+   * Creates a new ERROR log event.
+   *
+   * <p>
+   * The {@code source} and the {@code key} values are inferred from the method
+   * immediately calling this method.
+   *
+   * <p>
+   * The <strong>{@code source}</strong> value is the binary name of class
+   * enclosing the method calling this method.
+   *
+   * <p>
+   * The <strong>{@code key}</strong> value is a string containing the file
+   * name, a colon ':' and the line number where this method has been invoked.
+   *
+   * @param <T1> the type of the log argument
+   * @param <T2> the type of the second log argument
+   *
+   * @return a new ERROR log event
+   */
+  public static <T1, T2> Event2<T1, T2> error() {
+    return create(Level.ERROR, Event2::new);
+  }
+
+  /**
+   * Creates a new INFO log event.
+   *
+   * <p>
+   * The {@code source} and the {@code key} values are inferred from the method
+   * immediately calling this method.
+   *
+   * <p>
+   * The <strong>{@code source}</strong> value is the binary name of class
+   * enclosing the method calling this method.
+   *
+   * <p>
+   * The <strong>{@code key}</strong> value is a string containing the file
+   * name, a colon ':' and the line number where this method has been invoked.
+   *
+   * @param <T1> the type of the log argument
+   * @param <T2> the type of the second log argument
+   *
+   * @return a new INFO log event
+   */
+  public static <T1, T2> Event2<T1, T2> info() {
+    return create(Level.INFO, Event2::new);
+  }
+
+  /**
+   * Creates a new TRACE log event.
+   *
+   * <p>
+   * The {@code source} and the {@code key} values are inferred from the method
+   * immediately calling this method.
+   *
+   * <p>
+   * The <strong>{@code source}</strong> value is the binary name of class
+   * enclosing the method calling this method.
+   *
+   * <p>
+   * The <strong>{@code key}</strong> value is a string containing the file
+   * name, a colon ':' and the line number where this method has been invoked.
+   *
+   * @param <T1> the type of the log argument
+   * @param <T2> the type of the second log argument
+   *
+   * @return a new TRACE log event
+   */
+  public static <T1, T2> Event2<T1, T2> trace() {
+    return create(Level.TRACE, Event2::new);
+  }
+
+  /**
+   * Creates a new WARN log event.
+   *
+   * <p>
+   * The {@code source} and the {@code key} values are inferred from the method
+   * immediately calling this method.
+   *
+   * <p>
+   * The <strong>{@code source}</strong> value is the binary name of class
+   * enclosing the method calling this method.
+   *
+   * <p>
+   * The <strong>{@code key}</strong> value is a string containing the file
+   * name, a colon ':' and the line number where this method has been invoked.
+   *
+   * @param <T1> the type of the log argument
+   * @param <T2> the type of the second log argument
+   *
+   * @return a new WARN log event
+   */
+  public static <T1, T2> Event2<T1, T2> warn() {
+    return create(Level.WARN, Event2::new);
   }
 
 }
---
+++ logging/src/test/java/objectos/logging/Event2Test.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2021-2022 Objectos Software LTDA.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package objectos.logging;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+public class Event2Test {
+
+  final Event2<Void, Void> TRACE = Event2.trace();
+
+  final Event2<Void, Void> DEBUG = Event2.debug();
+
+  final Event2<Void, Void> INFO = Event2.info();
+
+  final Event2<Void, Void> WARN = Event2.warn();
+
+  final Event2<Void, Void> ERROR = Event2.error();
+
+  @Test
+  public void key() {
+    assertEquals(TRACE.key(), "Event2Test.java:24");
+    assertEquals(DEBUG.key(), "Event2Test.java:26");
+    assertEquals(INFO.key(), "Event2Test.java:28");
+    assertEquals(WARN.key(), "Event2Test.java:30");
+    assertEquals(ERROR.key(), "Event2Test.java:32");
+  }
+
+  @Test
+  public void level() {
+    assertEquals(TRACE.level(), Level.TRACE);
+    assertEquals(DEBUG.level(), Level.DEBUG);
+    assertEquals(INFO.level(), Level.INFO);
+    assertEquals(WARN.level(), Level.WARN);
+    assertEquals(ERROR.level(), Level.ERROR);
+  }
+
+  @Test
+  public void source() {
+    assertEquals(TRACE.source(), "objectos.logging.Event2Test");
+    assertEquals(DEBUG.source(), "objectos.logging.Event2Test");
+    assertEquals(INFO.source(), "objectos.logging.Event2Test");
+    assertEquals(WARN.source(), "objectos.logging.Event2Test");
+    assertEquals(ERROR.source(), "objectos.logging.Event2Test");
+  }
+
+}
