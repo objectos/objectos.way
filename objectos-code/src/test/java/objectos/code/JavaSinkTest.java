c4c8fb918e68a727615d1b2e80a58c0e28e0bb27
Marcio Endo <marcio@objectos.com.br>
2023-01-31 14:37:10 -0300

    [#120] pass JavaSinkTest TC05
    $source=5eac1e42575e7e908382610f27f4e43cc0814f03$

--- objectos-code/src/test/java/objectos/code/JavaSinkTest.java
+++ objectos-code/src/test/java/objectos/code/JavaSinkTest.java
@@ -187,7 +187,7 @@ public class JavaSinkTest {
     );
   }
 
-  @Test(enabled = false)
+  @Test
   public void testCase05() throws IOException {
     // @formatter:off
     var tmpl = new JavaTemplate() {
