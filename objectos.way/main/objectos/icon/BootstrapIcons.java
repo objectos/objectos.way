62d28cbc42f08671f8dec4e03f91d264841928ed
Marcio Endo <marcio@objectos.com.br>
2023-09-25 13:44:15 -0300

    [#501] add the x bootstrap icon
    Closes #501

--- objectos.way/main/objectos/icon/BootstrapIcons.java
+++ objectos.way/main/objectos/icon/BootstrapIcons.java
@@ -69,6 +69,19 @@ public class BootstrapIcons extends HtmlComponent {
     // @formatter:on
   }
 
+  /**
+   * Renders the <a href="https://icons.getbootstrap.com/icons/x/">x</a> icon.
+   *
+   * @return the {@code svg} element
+   */
+  public final ElementContents x() {
+    // @formatter:off
+    return icon(
+      path(d("M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"))
+    );
+    // @formatter:on
+  }
+
   private ElementContents icon(SvgInstruction... contents) {
     ElementContents svg;
     svg = svg(
