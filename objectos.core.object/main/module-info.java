e8252da2de6688edfa3817bb69b41164bbb04587
Marcio Endo <marcio@objectos.com.br>
2023-11-12 13:17:28 -0300

    build(object): move java files from way to core.object
    $source=8ec831ad0db49d9882a7f4dc7d1db73163d289c2$

--- objectos.core.object/main/module-info.java
+++ objectos.core.object/main/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022-2023 Objectos Software LTDA.
+ * Copyright (C) 2023 Objectos Software LTDA.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,22 +14,8 @@
  * limitations under the License.
  */
 /**
- * Defines the Objectos Way API.
+ * Utilities for {@code java.lang.Object} instances.
  */
-module objectos.way {
-	exports objectos.css;
-	exports objectos.css.tmpl;
-	exports objectos.css.util;
-	exports objectos.html;
-	exports objectos.html.tmpl;
-	exports objectos.http;
-	exports objectos.http.media;
-	exports objectos.http.server;
-	exports objectos.http.util;
-	exports objectos.icon;
-	exports objectos.js;
-	exports objectos.lang;
-	exports objectos.notes;
-
-	requires objectos.core.object;
+module objectos.core.object {
+	exports objectos.core.object;
 }
