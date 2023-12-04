a3a47a531cf614beb9543464c074f3ae86898402
Marcio Endo <marcio@objectos.com.br>
2023-12-04 13:19:05 -0300

    feat(http): add a Cookie parser
    re objectos/objectos#20
    
    $source=b58904fdbd3fd09b68163ff0deb0660f465d8155$

---
+++ objectos.http/test/objectos/http/CookieTest.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2023 Objectos Software LTDA.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package objectos.http;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+public class CookieTest {
+
+  @Test(description = """
+  It should parse a single name-value pair
+  """)
+  public void testCase01() {
+    Cookie c;
+    c = Cookie.parse("foo=bar");
+
+    assertEquals(c.get("foo"), "bar");
+  }
+
+}
