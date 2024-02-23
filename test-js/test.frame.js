1a30f0e6b5c0dd40cfeae8f6e6b73d9f4a7577cb
Marcio Endo <marcio@objectos.com.br>
2024-02-23 09:53:38 -0300

    test: nested data-frames with two children
    $source=a0272c7181cb903142656fa2c8ebf471919e9494$

--- test-js/test.frame.js
+++ test-js/test.frame.js
@@ -114,4 +114,131 @@ suite("Frame test", function() {
 		assert.equal(workArea().innerHTML, frame2.outerHTML);
 	});
 
+	test("nested data-frames (2 children): morph one child only", function() {
+		make(`
+		<div data-frame='root' data-frame-value='x'>
+    	<div data-frame='hd' data-frame-value='foo'>Foo</div>
+		<span>should remain</span>
+    	<form data-frame='x' data-frame-value='1' method='post' action='/test'>
+    	<button id='b' type='submit'>Before</button>
+    	</form>
+    	</div>`);
+		const page2 = makeElement(`
+		<div data-frame='root' data-frame-value='x'>
+    	<div data-frame='hd' data-frame-value='bar'>Bar</div>
+    	<form data-frame='x' data-frame-value='1' method='post' action='/test'>
+    	<button id='b' type='submit'>After</button>
+    	</form>
+    	</div>`);
+		const expect = makeElement(`
+		<div data-frame='root' data-frame-value='x'>
+    	<div data-frame='hd' data-frame-value='bar'>Bar</div>
+		<span>should remain</span>
+    	<form data-frame='x' data-frame-value='1' method='post' action='/test'>
+    	<button id='b' type='submit'>Before</button>
+    	</form>
+    	</div>`);
+
+		this.server.respondWith(
+			"POST",
+			"/test", [
+			200,
+			{ "Content-Type": "application/json" },
+			JSON.stringify([
+				{
+					cmd: "html",
+					value: page2.outerHTML
+				}
+			])
+		]);
+
+		const btn = byId("b");
+
+		btn.click();
+
+		this.server.respond();
+
+		assert.equal(workArea().innerHTML, expect.outerHTML);
+	});
+
+	test("nested data-frames (2 children): morph both children", function() {
+		make(`
+		<div data-frame='root' data-frame-value='x'>
+    	<div data-frame='hd' data-frame-value='foo'>Foo</div>
+		<span>should remain</span>
+    	<form data-frame='x' data-frame-value='1' method='post' action='/test'>
+    	<button id='b' type='submit'>Before</button>
+    	</form>
+    	</div>`);
+		const page2 = makeElement(`
+		<div data-frame='root' data-frame-value='x'>
+    	<div data-frame='hd' data-frame-value='bar'>Bar</div>
+    	<div data-frame='x' data-frame-value='2'>From form to div</div>
+    	</div>`);
+		const expect = makeElement(`
+		<div data-frame='root' data-frame-value='x'>
+    	<div data-frame='hd' data-frame-value='bar'>Bar</div>
+		<span>should remain</span>
+    	<div data-frame='x' data-frame-value='2'>From form to div</div>
+    	</div>`);
+
+		this.server.respondWith(
+			"POST",
+			"/test", [
+			200,
+			{ "Content-Type": "application/json" },
+			JSON.stringify([
+				{
+					cmd: "html",
+					value: page2.outerHTML
+				}
+			])
+		]);
+
+		const btn = byId("b");
+
+		btn.click();
+
+		this.server.respond();
+
+		assert.equal(workArea().innerHTML, expect.outerHTML);
+	});
+
+	test("nested data-frames (2 children): morph parent only", function() {
+		make(`
+		<div data-frame='root' data-frame-value='x'>
+    	<div data-frame='hd' data-frame-value='foo'>Foo</div>
+		<span>should not remain</span>
+    	<form data-frame='x' data-frame-value='1' method='post' action='/test'>
+    	<button id='b' type='submit'>Before</button>
+    	</form>
+    	</div>`);
+		const page2 = makeElement(`
+		<div data-frame='root' data-frame-value='y'>
+    	<div data-frame='hd' data-frame-value='foo'>Bar</div>
+    	<div data-frame='x' data-frame-value='1'>From form to div</div>
+    	</div>`);
+
+		this.server.respondWith(
+			"POST",
+			"/test", [
+			200,
+			{ "Content-Type": "application/json" },
+			JSON.stringify([
+				{
+					cmd: "html",
+					value: page2.outerHTML
+				}
+			])
+		]);
+
+		const btn = byId("b");
+
+		btn.click();
+
+		this.server.respond();
+
+		assert.equal(workArea().innerHTML, page2.outerHTML);
+	});
+
 });
