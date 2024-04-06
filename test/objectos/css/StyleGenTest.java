f2d9b23183cbfc5e6d49911c59d89e04e42b1610
Marcio Endo <marcio@objectos.com.br>
2024-04-06 09:11:55 -0300

    fix: StyleGen cursor utility
    $source=d7a38c9aa1d5a6d87a20149646d3311dbd634aa8$

--- test/objectos/css/StyleGenTest.java
+++ test/objectos/css/StyleGenTest.java
@@ -407,7 +407,7 @@ public class StyleGenTest {
     class Subject extends AbstractSubject {
       @Override
       final void classes() {
-        className("cursor-auto cursor-default cursor-pointer cursor-wait cursor-text cursor-move cursor-help cursor-not-allowed' cursor-none cursor-context-menu' cursor-progress cursor-cell cursor-crosshair cursor-vertical-text' cursor-alias cursor-copy cursor-no-drop' cursor-grab cursor-grabbing cursor-all-scroll' cursor-col-resize' cursor-row-resize' cursor-n-resize' cursor-e-resize' cursor-s-resize' cursor-w-resize' cursor-ne-resize' cursor-nw-resize' cursor-se-resize' cursor-sw-resize' cursor-ew-resize' cursor-ns-resize' cursor-nesw-resize' cursor-nwse-resize' cursor-zoom-in' cursor-zoom-out'");
+        className("cursor-auto cursor-default cursor-pointer cursor-wait cursor-text cursor-move cursor-help cursor-not-allowed cursor-none cursor-context-menu cursor-progress cursor-cell cursor-crosshair cursor-vertical-text cursor-alias cursor-copy cursor-no-drop cursor-grab cursor-grabbing cursor-all-scroll cursor-col-resize cursor-row-resize cursor-n-resize cursor-e-resize cursor-s-resize cursor-w-resize cursor-ne-resize cursor-nw-resize cursor-se-resize cursor-sw-resize cursor-ew-resize cursor-ns-resize cursor-nesw-resize cursor-nwse-resize cursor-zoom-in cursor-zoom-out");
       }
     }
 
@@ -422,35 +422,35 @@ public class StyleGenTest {
         .cursor-text { cursor: text }
         .cursor-move { cursor: move }
         .cursor-help { cursor: help }
-        .cursor-not-allowed' { cursor: not-allowed }
+        .cursor-not-allowed { cursor: not-allowed }
         .cursor-none { cursor: none }
-        .cursor-context-menu' { cursor: context-menu }
+        .cursor-context-menu { cursor: context-menu }
         .cursor-progress { cursor: progress }
         .cursor-cell { cursor: cell }
         .cursor-crosshair { cursor: crosshair }
-        .cursor-vertical-text' { cursor: vertical-text }
+        .cursor-vertical-text { cursor: vertical-text }
         .cursor-alias { cursor: alias }
         .cursor-copy { cursor: copy }
-        .cursor-no-drop' { cursor: no-drop }
+        .cursor-no-drop { cursor: no-drop }
         .cursor-grab { cursor: grab }
         .cursor-grabbing { cursor: grabbing }
-        .cursor-all-scroll' { cursor: all-scroll }
-        .cursor-col-resize' { cursor: col-resize }
-        .cursor-row-resize' { cursor: row-resize }
-        .cursor-n-resize' { cursor: n-resize }
-        .cursor-e-resize' { cursor: e-resize }
-        .cursor-s-resize' { cursor: s-resize }
-        .cursor-w-resize' { cursor: w-resize }
-        .cursor-ne-resize' { cursor: ne-resize }
-        .cursor-nw-resize' { cursor: nw-resize }
-        .cursor-se-resize' { cursor: se-resize }
-        .cursor-sw-resize' { cursor: sw-resize }
-        .cursor-ew-resize' { cursor: ew-resize }
-        .cursor-ns-resize' { cursor: ns-resize }
-        .cursor-nesw-resize' { cursor: nesw-resize }
-        .cursor-nwse-resize' { cursor: nwse-resize }
-        .cursor-zoom-in' { cursor: zoom-in }
-        .cursor-zoom-out' { cursor: zoom-out }
+        .cursor-all-scroll { cursor: all-scroll }
+        .cursor-col-resize { cursor: col-resize }
+        .cursor-row-resize { cursor: row-resize }
+        .cursor-n-resize { cursor: n-resize }
+        .cursor-e-resize { cursor: e-resize }
+        .cursor-s-resize { cursor: s-resize }
+        .cursor-w-resize { cursor: w-resize }
+        .cursor-ne-resize { cursor: ne-resize }
+        .cursor-nw-resize { cursor: nw-resize }
+        .cursor-se-resize { cursor: se-resize }
+        .cursor-sw-resize { cursor: sw-resize }
+        .cursor-ew-resize { cursor: ew-resize }
+        .cursor-ns-resize { cursor: ns-resize }
+        .cursor-nesw-resize { cursor: nesw-resize }
+        .cursor-nwse-resize { cursor: nwse-resize }
+        .cursor-zoom-in { cursor: zoom-in }
+        .cursor-zoom-out { cursor: zoom-out }
         """
     );
   }
