a685a960833ad01d4e1e4574f5ce8b844de2d09b
Marcio Endo <marcio@objectos.com.br>
2024-12-05 20:23:47 -0300

    feat(css): arbitrary length/percentage values for size #746
    Closes #746
    
    $source=d5fd68e379b39a350c7121b05e4bf3730c6fec41$

--- main/objectos/way/CssConfig.java
+++ main/objectos/way/CssConfig.java
@@ -771,90 +771,7 @@ final class CssConfig {
 
     // S
 
-    funcUtility(
-        Css.Key.SIZE,
-
-        values(
-            Css.Key.SIZE,
-
-            () -> Css.merge(
-                """
-                auto: auto
-                1/2: 50%
-                1/3: 33.333333%
-                2/3: 66.666667%
-                1/4: 25%
-                2/4: 50%
-                3/4: 75%
-                1/5: 20%
-                2/5: 40%
-                3/5: 60%
-                4/5: 80%
-                1/6: 16.666667%
-                2/6: 33.333333%
-                3/6: 50%
-                4/6: 66.666667%
-                5/6: 83.333333%
-                1/12: 8.333333%
-                2/12: 16.666667%
-                3/12: 25%
-                4/12: 33.333333%
-                5/12: 41.666667%
-                6/12: 50%
-                7/12: 58.333333%
-                8/12: 66.666667%
-                9/12: 75%
-                10/12: 83.333333%
-                11/12: 91.666667%
-                full: 100%
-                min: min-content
-                max: max-content
-                fit: fit-content
-                """,
-
-                spacing
-            )
-        ),
-
-        "size",
-
-        propertyType.height(),
-        propertyType.width()
-    );
-
-    colorUtility(
-        Css.Key.STROKE,
-
-        values(
-            Css.Key.STROKE,
-
-            () -> Css.merge(
-                """
-                none: none
-                """,
-
-                colors
-            )
-        ),
-
-        "stroke", "stroke"
-    );
-
-    funcUtility(
-        Css.Key.STROKE_WIDTH,
-
-        values(
-            Css.Key.STROKE_WIDTH,
-
-            """
-            0: 0
-            1: 1
-            2: 2
-            """
-        ),
-
-        "stroke", "stroke-width"
-    );
+    specS(colors);
 
     // T
 
@@ -2015,6 +1932,91 @@ final class CssConfig {
                 """
             )
         )
+    );
+  }
+
+  private void specS(Map<String, String> colors) {
+    funcUtility(
+        Css.Key.SIZE,
+
+        values(
+            Css.Key.SIZE,
+
+            """
+            auto: auto
+            1/2: 50%
+            1/3: 33.333333%
+            2/3: 66.666667%
+            1/4: 25%
+            2/4: 50%
+            3/4: 75%
+            1/5: 20%
+            2/5: 40%
+            3/5: 60%
+            4/5: 80%
+            1/6: 16.666667%
+            2/6: 33.333333%
+            3/6: 50%
+            4/6: 66.666667%
+            5/6: 83.333333%
+            1/12: 8.333333%
+            2/12: 16.666667%
+            3/12: 25%
+            4/12: 33.333333%
+            5/12: 41.666667%
+            6/12: 50%
+            7/12: 58.333333%
+            8/12: 66.666667%
+            9/12: 75%
+            10/12: 83.333333%
+            11/12: 91.666667%
+            full: 100%
+            min: min-content
+            max: max-content
+            fit: fit-content
+            """
+        ),
+
+        L_OR_P,
+
+        "size",
+
+        propertyType.height(),
+        propertyType.width()
+    );
+
+    colorUtility(
+        Css.Key.STROKE,
+
+        values(
+            Css.Key.STROKE,
+
+            () -> Css.merge(
+                """
+                none: none
+                """,
+
+                colors
+            )
+        ),
+
+        "stroke", "stroke"
+    );
+
+    funcUtility(
+        Css.Key.STROKE_WIDTH,
+
+        values(
+            Css.Key.STROKE_WIDTH,
+
+            """
+            0: 0
+            1: 1
+            2: 2
+            """
+        ),
+
+        "stroke", "stroke-width"
     );
   }
 
