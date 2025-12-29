c7973607739d03df80844a473762ace1e1e81a71
Marcio Endo <marcio@objectos.com.br>
2025-12-29 14:03:50 -0300

    wip(script): add script 009/010 tests #965
    $source=ad968b024a1bd875138fe6c79bb484d17053dc00$

---
+++ test/objectos/way/Script009Test.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2023-2025 Objectos Software LTDA.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package objectos.way;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+import static org.testng.Assert.assertEquals;
+
+import objectos.way.dev.Script009;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+@Listeners(Y.class)
+public class Script009Test {
+
+  @Test
+  public void action() {
+    assertEquals(
+        Script009.ACTION.toString(),
+
+        """
+        [["EI",[["JS","subject"]]],["PW","Element","textContent",[["GR"],["PR","Window","document"],["PR","Document","title"]]]]"""
+    );
+  }
+
+  @Test
+  public void live() {
+    try (var page = Y.page()) {
+      page.navigate("/script/009");
+
+      var clickMe = page.locator("#click-me");
+
+      var subject = page.locator("#subject");
+
+      assertThat(subject).hasText("Page title is...");
+
+      clickMe.click();
+
+      assertThat(subject).hasText("Script009");
+    }
+  }
+
+}
