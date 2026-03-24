9bdd455b036d58e0c318421604378ede94f9a947
Marcio Endo <marcio@objectos.com.br>
2026-03-24 11:50:21 -0300

    feat(http): request parse body v0.3.x #1024
    Closes #1024
    
    $source=60a0609fe92996b8c97d41632cc452cf1e548e90$

--- test/objectos/http/HttpRequestTest5ParseBody.java
+++ test/objectos/http/HttpRequestTest5ParseBody.java
@@ -21,13 +21,16 @@ import static org.testng.Assert.assertSame;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.UncheckedIOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import objectos.http.HttpRequestParser.InvalidRequestBody;
+import objectos.http.HttpRequestParser.InvalidRequestHeaders;
 import objectos.internal.Util;
 import objectos.way.Y;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class HttpRequestTest5ParseBody {
@@ -281,6 +284,328 @@ public class HttpRequestTest5ParseBody {
     );
 
     assertEquals(toByteArray(req), iso8859(content));
+  }
+
+  @Test(description = "file: client read IOException")
+  public void file02() {
+    final Tester tester;
+    tester = new Tester();
+
+    final String frag;
+    frag = ".".repeat(512);
+
+    final IOException ioe;
+    ioe = Y.trimStackTrace(new IOException(), 1);
+
+    try {
+      HttpRequestTester.parse(
+          test -> {
+            test.bodyFiles(tester);
+
+            test.bufferSize(128, 256);
+          },
+
+          """
+          POST / HTTP/1.1\r
+          Host: www.example.com\r
+          Content-Type: text/plain\r
+          Content-Length: 1024\r
+          \r
+          """,
+
+          frag, ioe
+      );
+
+      Assert.fail("It should have thrown");
+    } catch (IOException expected) {
+      assertSame(expected, ioe);
+    }
+  }
+
+  @Test(description = "file: ISE on getOutputStream")
+  public void file03() {
+    final IOException ioe;
+    ioe = Y.trimStackTrace(new IOException(), 1);
+
+    final Tester tester;
+    tester = new Tester() {
+      @Override
+      public final OutputStream newOutputStream(Path file) throws IOException {
+        throw ioe;
+      }
+    };
+
+    final String contents;
+    contents = ".".repeat(1024);
+
+    try {
+      HttpRequestTester.parse(
+          test -> {
+            test.bodyFiles(tester);
+
+            test.bufferSize(128, 256);
+          },
+
+          """
+          POST / HTTP/1.1\r
+          Host: www.example.com\r
+          Content-Type: text/plain\r
+          Content-Length: 1024\r
+          \r
+          """,
+
+          contents
+      );
+
+      Assert.fail("It should have thrown");
+    } catch (IOException expected) {
+      assertSame(expected, ioe);
+    }
+  }
+
+  @Test(description = "file: ISE on OutputStream.write")
+  public void file04() {
+    final IOException ioe;
+    ioe = Y.trimStackTrace(new IOException(), 1);
+
+    final Tester tester;
+    tester = new Tester() {
+      @Override
+      public final OutputStream newOutputStream(Path file) throws IOException {
+        return new OutputStream() {
+          @Override
+          public final void write(int b) throws IOException {
+            throw new UnsupportedOperationException("Implement me");
+          }
+
+          @Override
+          public final void write(byte[] b, int off, int len) throws IOException {
+            throw ioe;
+          }
+        };
+      }
+    };
+
+    final String contents;
+    contents = ".".repeat(1024);
+
+    try {
+      HttpRequestTester.parse(
+          test -> {
+            test.bodyFiles(tester);
+
+            test.bufferSize(128, 256);
+          },
+
+          """
+          POST / HTTP/1.1\r
+          Host: www.example.com\r
+          Content-Type: text/plain\r
+          Content-Length: 1024\r
+          \r
+          """,
+
+          contents
+      );
+
+      Assert.fail("It should have thrown");
+    } catch (IOException expected) {
+      assertSame(expected, ioe);
+    }
+  }
+
+  @Test(description = "file: ISE on OutputStream.close")
+  public void file05() {
+    final IOException ioe;
+    ioe = Y.trimStackTrace(new IOException(), 1);
+
+    final Tester tester;
+    tester = new Tester() {
+      @Override
+      public final OutputStream newOutputStream(Path file) throws IOException {
+        return new OutputStream() {
+          @Override
+          public final void close() throws IOException {
+            throw ioe;
+          }
+
+          @Override
+          public final void write(int b) throws IOException {}
+
+          @Override
+          public final void write(byte[] b, int off, int len) throws IOException {}
+        };
+      }
+    };
+
+    final String contents;
+    contents = ".".repeat(1024);
+
+    try {
+      HttpRequestTester.parse(
+          test -> {
+            test.bodyFiles(tester);
+
+            test.bufferSize(128, 256);
+          },
+
+          """
+          POST / HTTP/1.1\r
+          Host: www.example.com\r
+          Content-Type: text/plain\r
+          Content-Length: 1024\r
+          \r
+          """,
+
+          contents
+      );
+
+      Assert.fail("It should have thrown");
+    } catch (IOException expected) {
+      assertSame(expected, ioe);
+    }
+  }
+
+  @Test(description = "file: slow client")
+  public void file06() throws IOException {
+    final Tester tester;
+    tester = new Tester();
+
+    final String content;
+    content = ".o".repeat(512);
+
+    final HttpRequest req;
+    req = HttpRequestTester.parse(
+        test -> {
+          test.bodyFiles(tester);
+
+          test.bufferSize(2, 256);
+        },
+
+        """
+        POST / HTTP/1.1\r
+        Host: www.example.com\r
+        Content-Type: text/plain\r
+        Content-Length: 1024\r
+        \r
+        """,
+
+        content
+    );
+
+    assertEquals(toByteArray(req), iso8859(content));
+  }
+
+  @Test
+  public void longMaxValue() {
+    long parsed1 = Long.parseLong("9223372036854775807");
+    assertEquals(parsed1, 9_223_372_036_854_775_807L);
+
+    try {
+      Long.parseLong("9223372036854775808");
+      Assert.fail();
+    } catch (NumberFormatException expected) {
+
+    }
+  }
+
+  @DataProvider
+  public Object[][] badRequestProvider() {
+    return new Object[][] {
+        {
+            """
+            POST / HTTP/1.1\r
+            Host: www.example.com\r
+            Content-Type: text/plain\r
+            Content-Length: 5\r
+            Transfer-Encoding: chunked\r
+            \r
+            Uh-Oh
+            """,
+
+            InvalidRequestHeaders.BOTH_CL_TE,
+
+            "request contains both transfer-encoding and content-length"
+        },
+
+        {
+            """
+            POST / HTTP/1.1\r
+            Host: www.example.com\r
+            Content-Type: text/plain\r
+            Content-Length: five bytes\r
+            \r
+            Uh-Oh
+            """,
+
+            InvalidRequestHeaders.INVALID_CONTENT_LENGTH,
+
+            "request contains an invalid content-length"
+        },
+
+        {
+            """
+            POST / HTTP/1.1\r
+            Host: www.example.com\r
+            Content-Type: text/plain\r
+            \r
+            Uh-Oh
+            """,
+
+            InvalidRequestHeaders.LENGTH_REQUIRED,
+
+            "request requires content-length"
+        },
+
+        {
+            """
+            POST / HTTP/1.1\r
+            Host: host\r
+            Content-Type: text/plain; charset=utf-8\r
+            Content-Length: 9223372036854775808\r
+            \r
+            """,
+
+            InvalidRequestHeaders.CONTENT_TOO_LARGE,
+
+            "Content-Length unsigned long overflow"
+        },
+
+        {
+            """
+            POST / HTTP/1.1\r
+            Host: host\r
+            Content-Type: text/plain; charset=utf-8\r
+            Content-Length: 65\r
+            \r
+            """,
+
+            InvalidRequestHeaders.CONTENT_TOO_LARGE,
+
+            "Content-Length exceeds configured limit"
+        }
+
+    };
+  }
+
+  @SuppressWarnings("exports")
+  @Test(dataProvider = "badRequestProvider")
+  public void badRequest(String request, HttpClientException.Kind kind, String description) throws IOException {
+    try {
+      HttpRequestTester.parse(
+          test -> {
+            test.bufferSize(128, 128);
+
+            test.bodyMaxSize(64);
+          },
+
+          request
+      );
+
+      Assert.fail("It should have thrown");
+    } catch (HttpClientException expected) {
+      assertEquals(expected.kind, kind);
+    }
   }
 
   private byte[] toByteArray(HttpRequest req) {
