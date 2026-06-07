e63072002a83dce3161feaff47133966dbc1ab35
Marcio Endo <marcio@objectos.com.br>
2026-06-07 19:59:03 -0300

    wip(http): StaticFilesAttributes #1051
    $source=f99efe5990d213a9b1f6623b04b8c14d2589f77b$

---
+++ main/objectos/http/StaticFilesAttributes.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2023-2026 Objectos Software LTDA.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package objectos.http;
+
+import java.io.IOException;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+
+final class StaticFilesAttributes {
+
+  @FunctionalInterface
+  interface Reader {
+
+    BasicFileAttributes read(Path path) throws IOException;
+
+  }
+
+  private final Reader reader;
+
+  StaticFilesAttributes(Reader reader) {
+    this.reader = reader;
+  }
+
+  public final BasicFileAttributes read(Path path) throws IOException {
+    if (path != null) {
+      return read0(path);
+    } else {
+      return null;
+    }
+  }
+
+  private BasicFileAttributes read0(Path path) throws IOException {
+    try {
+      final BasicFileAttributes attrs;
+      attrs = reader.read(path);
+
+      if (attrs.isRegularFile()) {
+        return attrs;
+      } else {
+        return null;
+      }
+    } catch (NoSuchFileException e) {
+      return null;
+    }
+  }
+
+}
